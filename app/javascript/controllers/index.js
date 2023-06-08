da434b17fe4e12677abed33bc2c978c9ace5bb26
Florent S <sandri822@hotmail.fr>
2023-06-08 16:02:36 +0200

    ✨ install yarn

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,16 @@
 // Import and register all your controllers from the importmap under controllers/*
 
-import { application } from "controllers/application"
+import { application } from "controllers/application";
+
+import HelloControler from 'controllers/hello_controller';
+import CharacterCounter from 'stimulus-character-counter';
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
+eagerLoadControllersFrom("controllers", application);
+
+application.register('hello', HelloControler);
+application.register('character-counter', CharacterCounter);
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
