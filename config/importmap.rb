865920b6ffa0ea8b36c48e6e4909ba3009c9eafc
Florent S <sandri822@hotmail.fr>
2023-06-08 17:23:36 +0200

    ✨ Introduce character counter

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,6 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 
 pin 'popper', to: 'popper.js', preload: true
 pin 'bootstrap', to: 'bootstrap.min.js', preload: true
+pin 'stimulus-character-counter', to: 'https://ga.jspm.io/npm:stimulus-character-counter@4.2.0/dist/stimulus-character-counter.mjs'
 
 pin_all_from 'app/javascript/controllers', under: 'controllers'
